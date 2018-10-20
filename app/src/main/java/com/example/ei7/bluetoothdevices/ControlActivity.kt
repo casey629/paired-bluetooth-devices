e164fadada0cecf9c7df2c60c393316cd9bfc6ca
Casey Jane <caseyjane629@gmail.com>
2018-10-20 14:34:56 +0300

    initial commit

---
+++ app/src/main/java/com/example/ei7/bluetoothdevices/ControlActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ei7.bluetoothdevices
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class ControlActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
